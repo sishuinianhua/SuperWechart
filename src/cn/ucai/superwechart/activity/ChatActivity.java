4732ea77dd17d4a9bdc3eb4adce1c12a19c1a3a7
sishuinianhua <wdyzyr@qq.com>
2016-07-22 11:18:20 +0800

    更改聊天界面好友的昵称，在会话里显示聊天好友的头像和昵称

--- src/cn/ucai/superwechart/activity/ChatActivity.java
+++ src/cn/ucai/superwechart/activity/ChatActivity.java
@@ -390,7 +390,7 @@ public class ChatActivity extends BaseActivity implements OnClickListener, EMEve
 					((TextView) findViewById(R.id.name)).setText(toChatUsername);
 				}
 			}else{
-				UserUtils.setUserNick(toChatUsername, (TextView) findViewById(R.id.name));
+				UserUtils.setContactNick(toChatUsername, (TextView) findViewById(R.id.name));
 			}
 		} else {
 			// 群聊
